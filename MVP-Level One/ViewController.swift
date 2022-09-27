9045162b15ba4203ed18fe776c46f72c6d2d01d1
aliya <tlegen.aliya@gmail.com>
2022-09-27 21:16:36 +0600

    Initial Commit

---
+++ MVP-Level One/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP-Level One
+//
+//  Created by Алия Тлеген on 27.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
